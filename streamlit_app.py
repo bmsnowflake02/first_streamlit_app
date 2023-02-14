42955b130c06818d40cc07f2bf29e8b912189970
bmsnowflake02 <125397245+bmsnowflake02@users.noreply.github.com>
2023-02-14 12:41:43 -0600

    Update streamlit_app.py
    Set index to be fruit names instead of the list number

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas
 import streamlit
 
-streamlit.title('My Mom''s New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -12,5 +12,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
